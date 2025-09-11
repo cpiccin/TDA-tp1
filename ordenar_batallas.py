c44f155b71a173326c4f87d4e247db98d9b06668
agus <apuglisi@fi.uba.ar>
2025-09-11 03:04:43 -0300

    Se agrega algoritmo propuesto, mediciones

---
+++ ordenar_batallas.py
@@ -0,0 +1,12 @@
+def ordenar_batallas(tiempos, importancias):
+    n = len(tiempos)
+    batallas = []
+    
+    for i in range(n):
+        ratio = importancias[i] / tiempos[i]
+        batallas.append((i + 1, tiempos[i], importancias[i], ratio))
+    
+    batallas.sort(key=lambda x: x[3], reverse=True)
+    return batallas
+
+print(ordenar_batallas([3, 1, 2], [5, 10, 15]))
